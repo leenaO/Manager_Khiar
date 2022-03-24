2920df670cdfbabd0062a3fbe8deb922549cb4e9
jewellery <53010988+jawaher1998@users.noreply.github.com>
2022-03-24 10:30:39 +0300

    jawaher

--- app/src/main/java/com/example/manag/KhiarAddPage.java
+++ app/src/main/java/com/example/manag/KhiarAddPage.java
@@ -1,16 +1,41 @@
 package com.example.manag;
 
+import static android.Manifest.permission.CAMERA;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Camera;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.MenuItem;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -18,9 +43,20 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -42,11 +78,22 @@ public class KhiarAddPage extends AppCompatActivity {
     Product product;
     TextView productIngredientsTextView;
     String productIngredients;
-    ImageButton ingredientsBtn;
     boolean keto;
     boolean sugarFree;
     boolean vegan;
     int BACK =1;
+    ImageView ingredientsBtn;
+    private ImageView front_imageV;
+    private RelativeLayout frontP, backP;
+    Uri uri;
+    static final int CAMERA_REQUEST_CODE= 200;
+    static final int STORAGE_REQUEST_CODE=400;
+    static final int IMAGE_PICK_GALLERY_CODE=1000;
+    static final int IMAGE_PICK_CAMERA_CODE=1001;
+    StorageReference storageReference;
+    String cameraPermission[] = {Manifest.permission.CAMERA};
+    String storagePermission[];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -69,12 +116,53 @@ public class KhiarAddPage extends AppCompatActivity {
         section=(Spinner) findViewById(R.id.spinner);
         productIngredientsTextView=(TextView)findViewById(R.id.ProductIngredientsTV);
         add=(Button)findViewById(R.id.addp);
-        ingredientsBtn=(ImageButton)findViewById(R.id.bPicture);
+        ingredientsBtn = (ImageView) findViewById(R.id.bPicture);
+        front_imageV = (ImageView)findViewById(R.id.fPicture);
 //        productIngredients=getIntent().getStringExtra("IngredientsKey");
 //        productIngredientsTextView.setText(productIngredients);
+        storageReference = FirebaseStorage.getInstance().getReference("Products_Manger/");
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(name.getText().toString().isEmpty()){
+                    name.setError("Is empty");
+                    return;
+                }
+                if(name.getText().toString().contains("[0-9]+")){
+                    Toast.makeText(getApplicationContext(),"Name Incorrect input! should be letters",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(price.getText().toString().isEmpty()){
+                    price.setError("The filed is empty");
+                    return;
+                }
+                if(price.getText().toString().contains("[a-zA-Z]+")){
+                    price.setError("Incorrect input! should be number");
+                    return;
+                }
+
+                if(price.getText().toString().length()<=0 ){
+                    Toast.makeText(getApplicationContext(),"Price Incorrect input! should be great 0",Toast.LENGTH_LONG).show();
+                    return;
+                }  if(amount.getText().toString().length()<=0 ){
+                    Toast.makeText(getApplicationContext(),"Amount Incorrect input! should be great 0",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if( amount.getText().toString().contains("-")){
+                    Toast.makeText(getApplicationContext(),"Amount Incorrect input! should be great 0",Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+
+                if( amount.getText().toString().isEmpty()){
+                    amount.setError("The filed is empty");
+                    return;
+                }
+                if(price.getText().toString().isEmpty() || amount.getText().toString().isEmpty() ){
+                    Toast.makeText(getApplicationContext(),"The filed is empty",Toast.LENGTH_LONG).show();
+                    return;
+                }
+
                 productInsert();
             }
         });
@@ -89,50 +177,81 @@ public class KhiarAddPage extends AppCompatActivity {
 
 
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==BACK){
-            if(resultCode==RESULT_OK){
-                productIngredients=data.getStringExtra("IngredientsKey");
-                productIngredientsTextView.setText(productIngredients);
-            }
-        }
+    /**file extinction jpg , png,... **/
+    private String getfileextiontio(Uri u){
+        ContentResolver resolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(resolver.getType(u));
     }
+
+
 
     private void productInsert() {
-        Map<String,Object> map= new HashMap<>();
-        map.put("name",name.getText().toString());
-        map.put("price",price.getText().toString());
-        map.put("amount",amount.getText().toString());
-        map.put("section",section.getSelectedItem().toString());
-        map.put("ingredients",productIngredientsTextView.getText().toString());
-        dietSection();
-        map.put("keto",keto);
-        map.put("sugarFree",sugarFree);
-        map.put("vegan",vegan);
-        FirebaseDatabase.getInstance().getReference().child("Products").push().setValue(map)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void unused) {
-                        name.setText("");
-                        price.setText("");
-                        amount.setText("");
-                        productIngredientsTextView.setText("");
-                        Toast.makeText(getApplicationContext(), "Product successfully added", Toast.LENGTH_SHORT).show();
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(getApplicationContext(), "Could not added", Toast.LENGTH_SHORT).show();
+        if(uri != null){
+            StorageReference storageReference1 = storageReference.child(System.currentTimeMillis()+"."+getfileextiontio(uri));
+            storageReference1.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    storageReference1.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
 
-            }
-        });
+                            Map<String, Object> map = new HashMap<>();
+                            map.put("name", name.getText().toString());
+                            map.put("price", price.getText().toString());
+                            map.put("amount", amount.getText().toString());
+                            map.put("section", section.getSelectedItem().toString());
+                            map.put("ingredients", productIngredientsTextView.getText().toString());
+                            dietSection();
+                            map.put("keto",keto);
+                            map.put("sugarFree",sugarFree);
+                            map.put("vegan",vegan);
+                            map.put("image",String.valueOf(uri));
+
+
+                            FirebaseDatabase.getInstance().getReference().child("Products").push().setValue(map)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void unused) {
+                                            name.setText("");
+                                            price.setText("");
+                                            amount.setText("");
+                                            productIngredientsTextView.setText("");
+                                            front_imageV.setImageURI(null);
+                                            Toast.makeText(getApplicationContext(), "Product successfully added", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(getApplicationContext(), "Could not added"+e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                                }
+                            });
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        }else { Toast.makeText(getApplicationContext(),"pleas add one image",Toast.LENGTH_LONG).show();
+        }
+
 
     }
-    public void ProductIngredients(View v){
-        Intent ingredientsImg = new Intent(this,ingredientsProductImg.class);
+
+    public void ProductIngredients(View v) {
+        Intent ingredientsImg = new Intent(this, ingredientsProductImg.class);
         startActivity(ingredientsImg);
     }
     public void dietSection(){
@@ -140,12 +259,12 @@ public class KhiarAddPage extends AppCompatActivity {
         sugarFree=true;
         vegan=true;
         /**String[] ka={"apple","orange","dates","mango","pomegranate","banana","dried fruit","grape","kiwi","peach","fig","cantaloupe","pineapple","pear","raisin",
-                "potatoes","sweet potatoes","baked potatoes","corn","peas","carrot","yam", "avocado oil",
-                "rice", "wheat", "oats", "barley","quinoa","kale",
-                "bread","corn flakes","pasta", "pizza","popcorn",
-                "Beans"};
-        String[] la={"soybean oil","olive oil","coconut oil"};
-        String[] va={"milk"};*/
+         "potatoes","sweet potatoes","baked potatoes","corn","peas","carrot","yam", "avocado oil",
+         "rice", "wheat", "oats", "barley","quinoa","kale",
+         "bread","corn flakes","pasta", "pizza","popcorn",
+         "Beans"};
+         String[] la={"soybean oil","olive oil","coconut oil"};
+         String[] va={"milk"};*/
         String line="";
         String line2="";
         String line3="";
@@ -178,8 +297,8 @@ public class KhiarAddPage extends AppCompatActivity {
         }catch(IOException e){}
 
         /**ArrayList<String> ketoAvoid=new ArrayList<>(Arrays.asList(ka));
-        ArrayList<String> lowAvoid=new ArrayList<>(Arrays.asList(sa));
-        ArrayList<String> veganvoid=new ArrayList<>(Arrays.asList(va));*/
+         ArrayList<String> lowAvoid=new ArrayList<>(Arrays.asList(sa));
+         ArrayList<String> veganvoid=new ArrayList<>(Arrays.asList(va));*/
         String[] sugarfreeDrinksAvoid={"Blueberry", "Caramel", "Chai" , "Chamomile", "Chocolate", "Cinnamon", "Cranberry", "Echinacea"};
 
 
@@ -244,5 +363,144 @@ public class KhiarAddPage extends AppCompatActivity {
 
         }
 
+    }
+
+
+    // add photo
+    private void showImageImportDialog() {
+        String[] items = {"Camera", "Gallery"};
+        androidx.appcompat.app.AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+
+        dialog.setTitle("select Image");
+        dialog.setItems(items, new DialogInterface.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (i ==0){
+                    //camera
+                    if(!checkCameraPermission()) {
+                        requestCameraPermission();
+                        pickCamera();
+                    }
+                }else if(i==1){
+                    //gallery
+                    if(!checkStoragePermission()){
+
+                        pickGallery();
+                    }
+                }
+            }
+        });
+        dialog.create().show();
+    }
+
+    private void pickGallery() {
+        Intent intent =new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_GALLERY_CODE);
+
+    }
+    private void requestCameraPermission() {
+        if (ContextCompat.checkSelfPermission(KhiarAddPage.this, Manifest.permission.CAMERA)== PackageManager.PERMISSION_DENIED)
+            ActivityCompat.requestPermissions(this,cameraPermission,CAMERA_REQUEST_CODE);
+    }
+
+    private void pickCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "new-photo-name.jpg");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Image capture by camera");
+        uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        startActivityForResult(cameraIntent, IMAGE_PICK_CAMERA_CODE);
+    }
+
+
+
+    private boolean checkStoragePermission() {
+        boolean result= ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)==(PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+
+
+
+    private boolean checkCameraPermission() {
+        boolean result = ContextCompat.checkSelfPermission(this, CAMERA)==(PackageManager.PERMISSION_GRANTED);
+        boolean result1 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)==(PackageManager.PERMISSION_GRANTED);
+
+        return result && result1;
+
+    }
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        switch (requestCode) {
+//            case CAMERA_REQUEST_CODE:
+//                if (grantResults.length > 0) {
+//                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+//                    boolean writeStorageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+//                    if (cameraAccepted && writeStorageAccepted) {
+//                        pickCamera();
+//                    } else {
+//                        Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+//                    }
+//                }
+//                break;
+//            case STORAGE_REQUEST_CODE:
+//                if (grantResults.length > 0) {
+//                    boolean writeStorageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+//                    if (writeStorageAccepted) {
+//                        pickGallery();
+//                    } else {
+//                        Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+//                    }
+//                }
+//                break;
+//        }
+//    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==BACK){
+            if(resultCode==RESULT_OK){
+                productIngredients=data.getStringExtra("IngredientsKey");
+                productIngredientsTextView.setText(productIngredients);
+            }
+        }
+        if (resultCode == RESULT_OK) {
+            if (requestCode == IMAGE_PICK_GALLERY_CODE) {
+                uri = data.getData();
+                front_imageV.setImageURI(uri);
+
+            }
+            if (requestCode == IMAGE_PICK_CAMERA_CODE) {
+                try {
+                    ContentResolver cr = getContentResolver();
+                    try {
+                        // Creating a Bitmap with the image Captured
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(cr, uri);
+                        // Setting the bitmap as the image of the
+                        front_imageV.setImageBitmap(bitmap);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } catch (IllegalArgumentException e) {
+                    if (e.getMessage() != null)
+                        Log.e("Exception", e.getMessage());
+                    else
+                        Log.e("Exception", "Exception");
+                    e.printStackTrace();
+
+                }
+            }
+        }
+    }
+
+    // when click card relative
+    public void FP(View view) {
+        showImageImportDialog();
     }
 }
