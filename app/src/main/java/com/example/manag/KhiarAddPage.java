ce802c63c6cdc523842fcc399b52c0831233ef3c
jewellery <53010988+jawaher1998@users.noreply.github.com>
2022-03-28 19:59:26 +0300

    jawaher

--- app/src/main/java/com/example/manag/KhiarAddPage.java
+++ app/src/main/java/com/example/manag/KhiarAddPage.java
@@ -165,6 +165,7 @@ public class KhiarAddPage extends AppCompatActivity {
                 }
 
                 productInsert();
+                Toast.makeText(getApplicationContext(),"add product successfully",Toast.LENGTH_LONG).show();
             }
         });
         ingredientsBtn.setOnClickListener(new View.OnClickListener() {
@@ -208,7 +209,6 @@ public class KhiarAddPage extends AppCompatActivity {
                             map.put("sugarFree",sugarFree);
                             map.put("vegan",vegan);
                             map.put("image",String.valueOf(uri));
-
 
                             FirebaseDatabase.getInstance().getReference().child("Products").push().setValue(map)
                                     .addOnSuccessListener(new OnSuccessListener<Void>() {
